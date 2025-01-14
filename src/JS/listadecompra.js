4b5ac92adbf2d62bf1541f6f14d0636b22985aa9
costantinav <costantinaviolante@gmail.com>
2025-01-14 15:51:52 +0100

    new

---
+++ src/JS/listadecompra.js
@@ -0,0 +1,33 @@
+// Add current article to shopping list
+function addToShoppingList() {
+    let articleName = document.getElementById('aricle').value
+     let articleQty = document.getElementById('qty').value
+  let articlePrice = document.getElementById('price').value
+  let articulo = { qty: 1, name: newArticle, price: 10 }
+}
+
+//Objects
+let productInformation = {
+  qty: 0,
+  name: '',
+  price: 0
+}
+
+let shoppingListWithObjects = [
+  {
+    qty: 1,
+    name: 'carne',
+    price: 10
+  },
+  {
+    qty: 2,
+    name: 'fruta',
+    price: 2
+  },
+  {
+    qty: 3,
+    name: 'pescado',
+    price: 20
+  }
+]
+
